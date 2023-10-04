bddd80edfea8e919517a1133b153feab8725a099
theprateekgoyal2 <prateekgoyal6417@gmail.com>
2023-10-04 17:30:26 +0530

    settings.py-2

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -135,11 +135,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# print(STATIC_ROOT)
 
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
