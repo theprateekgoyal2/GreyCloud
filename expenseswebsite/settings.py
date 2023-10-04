a718abfe8baa50aca801891562455cb8691f71ec
theprateekgoyal2 <prateekgoyal6417@gmail.com>
2023-10-04 17:27:27 +0530

    settings.py update-1

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # print(STATIC_ROOT)
 
