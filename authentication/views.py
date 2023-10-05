d7a6d0ecf5102efde056953326a8d0505c9ea3a0
theprateekgoyal2 <prateekgoyal6417@gmail.com>
2023-10-05 11:10:13 +0530

    send email change

--- authentication/views.py
+++ authentication/views.py
@@ -77,7 +77,7 @@ class RegistrationView(View):
                 })
                 activate_url = 'http://'+domain+link
                 
-                email_body = 'Hi '+user.username + \
+                email_body = 'Hi '+user.first_name + " " + user.last_name +  \
                     ' Please use this link to verify your account\n' +activate_url    
                 send_verification_mail(email, email_body)
                 messages.success(request, 'Account successfully created')
